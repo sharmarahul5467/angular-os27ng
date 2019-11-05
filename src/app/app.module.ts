b320b51d0f983cd8ead4ca57adb5ce4582a53afc
sharmarahul5467 <sharma.rahul5467@gmail.com>
2019-11-05 19:54:05 +0530

    4 files modified, 12 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,13 @@ import { HeaderComponent } from './header/header.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
 import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent, RecipeComponent, RecipeDetailComponent, RecipeListComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, RecipeComponent, RecipeDetailComponent, RecipeListComponent, ShoppingListComponent, ShoppingEditComponent, RecipeItemComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
